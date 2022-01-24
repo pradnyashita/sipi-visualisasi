f2fa320adf00e441cae7945d8d2065104c30da33
naufal sunandar <65304518+icedmuffin@users.noreply.github.com>
2022-01-24 22:00:06 +0700

    ngubah tittle

--- idl-kampung.php
+++ idl-kampung.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js"></script>
-  <title>Document</title>
+  <title>Ini punya sapa</title>
 </head>
 <body onresize="shiftBarColumns()">
 
